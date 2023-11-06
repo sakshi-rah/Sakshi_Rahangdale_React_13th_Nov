07b7581015a46b347439486bc7866f2be98de8b1
Sakshi Rahangdale <rahangdalesakshi2@gmail.com>
2023-11-06 11:34:00 +0530

    task react app setup

---
+++ reacttask/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import Cryptodataapi from './Component/Cryptodataapi';
+function App() {
+  return (
+    <div className="App">
+     <Cryptodataapi/>
+    </div>
+  );
+}
+
+export default App;
---
+++ reacttask/src/Component/Cryptodataapi.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react'
+import axios from 'axios'
+function Cryptodataapi() {
+    const[data, setData] = useState([]);
+    const fetchData = async()=>{
+        
+    }
+    return (
+        <div>
+            <h1>Crypto Markets Coin API</h1>
+        </div>
+    )
+}
+
+export default Cryptodataapi
